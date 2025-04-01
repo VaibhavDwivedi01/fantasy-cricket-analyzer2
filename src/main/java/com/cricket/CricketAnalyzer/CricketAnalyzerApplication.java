179b5c5588539e4ebf6073577384ea3d5d44c546
VaibhavDwivedi01 <vaibhavdwivedi0249@gmail.com>
2025-04-01 23:09:20 +0530

    Initial commit

---
+++ src/main/java/com/cricket/CricketAnalyzer/CricketAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.cricket.CricketAnalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricketAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricketAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cricket/CricketAnalyzer/CricketAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cricket.CricketAnalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricketAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
